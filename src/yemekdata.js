36b5f77bb555506ab103c9652f369c9a7d53b9ba
aylineskiturk <aylineskiturk987@gmail.com>
2023-11-04 11:32:17 +0300

    food-menu

---
+++ src/yemekdata.js
@@ -0,0 +1,134 @@
+const menuler = [
+  {
+    ad: "Whooper Menü",
+    ozellik: [("small", "medium", "large")],
+    fiyat: [
+      {
+        small: 150,
+        medium: 170,
+        large: 220,
+      },
+    ],
+    img: "https://www.burgerking.com.tr/cmsfiles/products/double-whopper-menu.png?v=296",
+    desc: "Özel kampanyalı menüler",
+  },
+  {
+    ad: "Rodeo Menü",
+    ozellik: [("small", "medium", "large")],
+    fiyat: [
+      {
+        small: 100,
+        medium: 130,
+        large: 170,
+      },
+    ],
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSae6ueoulaYoY6LbybxOBs1GgVK5XN8iRKsg&usqp=CAU",
+    desc: "Özel kampanyalı menüler",
+  },
+  {
+    ad: "king beef menü",
+    ozellik: [("small", "medium", "large")],
+    fiyat: [
+      {
+        small: 150,
+        medium: 180,
+        large: 230,
+      },
+    ],
+    img: "https://saraylidoner.com/wp-content/uploads/2021/09/dublex.jpg",
+    desc: "Özel kampanyalı menüler",
+  },
+  {
+    ad: "Big king menu",
+    ozellik: [("small", "medium", "large")],
+    fiyat: [
+      {
+        small: 90,
+        medium: 135,
+        large: 200,
+      },
+    ],
+    img: "https://www.burgerking.com.tr/cmsfiles/products/double-texas-smokehouse-burger-menu.png?v=296",
+    desc: "Özel kampanyalı menüler",
+  },
+  {
+    ad: "Chicken royal",
+    ozellik: [("small", "medium", "large")],
+    fiyat: [
+      {
+        small: 100,
+        medium: 120,
+        large: 150,
+      },
+    ],
+    img: "https://www.burgerking.com.tr/cmsfiles/campaigns/burger-king-den-lezzetler-ligi-menusu.jpg?v=296",
+    desc: "Özel kampanyalı menüler",
+  },
+  {
+    ad: " Fish Royale menü",
+    ozellik: [("small", "medium", "large")],
+    fiyat: [
+      {
+        small: 80,
+        medium: 110,
+        large: 140,
+      },
+    ],
+    img: "https://cajuncorner.co/wp-content/uploads/2023/05/Cajun-Burger.jpg",
+    desc: "Özel kampanyalı menüler",
+  },
+  {
+    ad: "Menü 7",
+    ozellik: [("small", "medium", "large")],
+    fiyat: [
+      {
+        small: 150,
+        medium: 170,
+        large: 220,
+      },
+    ],
+    img: "https://cdn.yemek.com/uploads/2023/11/arjantin-burger-menu-2.jpg",
+    desc: "Özel kampanyalı menüler",
+  },
+  {
+    ad: "Menü 8",
+    ozellik: [("small", "medium", "large")],
+    fiyat: [
+      {
+        small: 150,
+        medium: 170,
+        large: 220,
+      },
+    ],
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbNXXuikTahbysy4RZsyQ0I6CUKzTiVmv8PQ&usqp=CAU",
+    desc: "Özel kampanyalı menüler",
+  },
+  {
+    ad: "Texas smoke menu",
+    ozellik: [("small", "medium", "large")],
+    fiyat: [
+      {
+        small: 150,
+        medium: 170,
+        large: 220,
+      },
+    ],
+    img: "https://www.burgerking.com.tr/cmsfiles/campaigns/2li-big-king-firsati.jpg?v=296",
+    desc: "Özel kampanyalı menüler",
+  },
+  {
+    ad: "Hamburger menü",
+    ozellik: [("small", "medium", "large")],
+    fiyat: [
+      {
+        small: 150,
+        medium: 170,
+        large: 220,
+      },
+    ],
+    img: "https://www.burgerking.com.tr/cmsfiles/categories/urunler/kids-menuler.png?v=296",
+    desc: "Özel kampanyalı menüler",
+  },
+];
+
+export default menuler;
